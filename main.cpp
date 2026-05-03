b7e68a35b2079a036e1673fabe8135e8cd03a7ee
Ricardo de Castro Soares <ricardodecastrosoares@gmail.com>
2026-05-02 23:24:46 -0300

    Adicionando o primeiro combate e o primeiro inimigo

--- main.cpp
+++ main.cpp
@@ -4,15 +4,26 @@
 #include <ctime>
 using namespace std;
 
+int zumbiAtaque1(int forca_zumbi1)
+{
+	int ataque_zumbi1{};
+	
+	ataque_zumbi1 = forca_zumbi1 + (rand() % 6) + 1;
+	
+	return ataque_zumbi1;
+}
+
 int main()
 {
 	srand(time(0));
 	
 	string nome_personagem;
+	const int classeArmadura_personagem = 10, classeArmadura_zumbi1 = 8, forca_zumbi1 = 14, destreza_zumbi1 = 4;
 	bool inicioDojogo = false;
-	int opcao{}, vida_personagem = 10, perdendo_vida{};
+	int opcao{}, vida_personagem = 10, vida_zumbi1 = 8, progresso{}, escolha{};
+	int dano_zumbi1 = zumbiAtaque1(forca_zumbi1);
 	char opcao2;
-	double forca_personagem{}, destreza_personagem{};
+	double forca_personagem{}, destreza_personagem{}, defesa_personagem{};
 	
 	do{
 		cout<<"\n=== MENU ===\n";
@@ -26,11 +37,12 @@ int main()
 			cout<<"\nOpcão invalida!! Digite entre [1 e 2]\n";
 		}
 		
-		switch(opcao){
+		switch(opcao){// switch com 2 opções, entrar no jogo e sair do jogo
 			case 1:
 				cin.ignore();
 				system("cls");
 				
+				vida_zumbi1 = 8;
 				vida_personagem = 10;
 				inicioDojogo = false;
 				
@@ -51,7 +63,7 @@ int main()
 				cout<<"\nDestreza: "<<destreza_personagem;
 				
 					switch(i){
-						case 1:
+						case 1:// Distribui 2 atributos aleatorios sendo que o usuario possui 3 tentativas 
 							cout<<"\n\nDeseja continuar com esses atributos? [S/N]: ";
 							cin>>opcao2;
 							if(opcao2 == 'S' || opcao2 == 's'){
@@ -85,28 +97,53 @@ int main()
 					
 					system("cls");
 												
-				if(inicioDojogo == true){
+				if(inicioDojogo == true){// Entra no jogo e montra os atributos gerados aleatorios aceito pelo usuario
 					cout<<"\n======= ESSES SERÃO SEU NOME E ATRIBUTOS =======\n";
 					cout<<"\nNome do personagem: "<<nome_personagem;
 					cout<<"\nForça: "<<forca_personagem;
-					cout<<"\nDestreza: "<<destreza_personagem; cout<<"\n\n";
+					cout<<"\nDestreza: "<<destreza_personagem;
+					cout<<"\nClasse de Armadura: "<<classeArmadura_personagem; cout<<"\n\n";
 					
 					system("pause");
 					system("cls");
 					
-					while(vida_personagem > 0){
-						cout<<"Escolha um caminho [1 Floresta/ 2 Cidade]: ";
-						cin>>perdendo_vida;
-						switch(perdendo_vida){//esse blocofoi apenas um teste
-							case 1:
-								vida_personagem = 0;
-								cout<<"Você morreu!!\n\n";
-								system("pause");							
-								break;
-							case 2:
-								cout<<"Você chegou na cidade\n\n";
-								system("pause");
-							break;
+					while(vida_personagem > 0){// aqui é onde vai sempre verificar se a vida do personagem estar abaixo de 0 sendo sua vida maxima 10
+						if(progresso == 0){
+							cout<<"Você esta no portão da cidade e tem dois caminhos...\n\n";
+							system("pause");
+							system("cls");
+							
+							cout<<"Escolha um caminho [1 Floresta/ 2 Planíce]: ";
+							cin>>escolha;
+							
+							if(escolha == 1) progresso = 1;
+							if(escolha == 2) progresso = 2;
+							
+						}else if(progresso == 1){
+							cout<<"Você chegou na floresta, e de repente aparece 2 zumbi e não a mais como voltar atrás, um dos zumbi te da um golpe...\n\n";
+							
+							while(vida_zumbi1 > 0 && vida_personagem > 0){
+								defesa_personagem = classeArmadura_personagem + (rand() % 20) + 1;
+								dano_zumbi1 = zumbiAtaque1(forca_zumbi1);
+								
+								if(dano_zumbi1 > defesa_personagem){
+									vida_personagem = vida_personagem - dano_zumbi1;
+									if(vida_personagem < 0) vida_personagem = 0;
+								
+									cout<<"VOCÊ TOMOU DANO... - "<<vida_personagem<<" DE VIDA";
+									
+									system("pause");
+									
+									//if()//apenas para saber de onde vou continuar o codigo
+								}else{
+									system("cls");
+								
+									cout<<"\nO zumbi errou...";
+								
+									cout<<"\nde um contra ataque";//(cin) apenas para parar o loop
+									cin>>escolha;
+								}
+							}
 						}
 					}
 				}
@@ -114,7 +151,7 @@ int main()
 				
 			case 2:
 				system("cls");
-				cout<<"\nAté logo!!\n\n";
+				cout<<"\nAté !!\n\n";
 				system("pause");
 				break;		
 			}
